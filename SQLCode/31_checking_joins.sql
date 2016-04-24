feaa16d62262ca6830475a5d75518d4b36f5cdb9
way0utwest <sjones@dkranch.net>
2016-04-24 12:34:03 -0600

    stub for demos
    Stub files

---
+++ SQLCode/31_checking_joins.sql
@@ -0,0 +1,47 @@
+/*
+Testing T-SQL Made Easy - Checking Joins
+
+Steve Jones, copyright 2016
+
+This code is provided as is for demonstration purposes. It may not be suitable for
+your environment. Please test this on your own systems. This code may not be republished 
+or redistributed by anyone without permission.
+You are free to use this code inside of your own organization.
+*/
+
+-- We want to alter a table.
+USE [TestingTSQL]
+GO
+
+-- we have a procedure using a join
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- test happy path
+
+
+
+
+
+
+-- alter procedure to a left join
+
+
+
+
+
+-- check the tests
+EXEC tsqlt.run '[].[]';
---
+++ SQLCode/61_heuristics.sql
@@ -0,0 +1,15 @@
+/*
+Testing T-SQL Made Easy - Tests can find potential holes in requirements
+
+Steve Jones, copyright 2016
+
+This code is provided as is for demonstration purposes. It may not be suitable for
+your environment. Please test this on your own systems. This code may not be republished 
+or redistributed by anyone without permission.
+You are free to use this code inside of your own organization.
+*/
+
+-- We want to alter a table.
+USE [TestingTSQL]
+GO
+
